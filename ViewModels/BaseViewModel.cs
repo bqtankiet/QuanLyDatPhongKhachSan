d6d046b4262afc59912530dec49341c8d52ea563
DuyBao16 <22130020@st.hcmuaf.edu.vn>
2024-12-26 15:38:52 +0700

    Add files via upload

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,16 +1,31 @@
 ﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
-namespace QLKS_CK.ViewModels
+namespace project_v3.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        // Sự kiện để thông báo thay đổi thuộc tính
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        // Phương thức để thông báo thay đổi thuộc tính
+        protected void OnPropertyChanged(string propertyName)
         {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Thuộc tính mẫu (có thể thêm nhiều thuộc tính khác)
+        private string _someProperty;
+        public string SomeProperty
+        {
+            get => _someProperty;
+            set
+            {
+                if (_someProperty != value)
+                {
+                    _someProperty = value;
+                    OnPropertyChanged(nameof(SomeProperty));  // Thông báo thay đổi
+                }
+            }
         }
     }
 }
